75907577f871e411023bc75f8f4d84cba6ff796a
alyarusydam@gmail.com <alyarusydam@gmail.com>
2024-07-13 17:38:54 +0700

    change title and icon

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Index from "./pages";
 import Detail from "./pages/id";
 import { TranslationProvider } from "./components/TranslationContext";
 import Footer from "./components/Footer";
-// import Checkout from "./components/Checkout";
 
 const App = () => {
   return (
@@ -14,7 +13,6 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/:id" element={<Detail />} />
-            {/* <Route path="/checkout" element={<Checkout/>} /> */}
           </Routes>
         </Router>
         <Footer/>
